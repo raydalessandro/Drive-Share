e2ab0b136e8f9b17abd3e1e60b404cb47683ba2a
raydalessandro <alessio.marrone1993@gmail.com>
2025-11-20 22:33:16 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import Routes from './pages/Routes'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -16,6 +15,7 @@ import Leaderboard from "./pages/Leaderboard";
 import Friends from "./pages/Friends";
 import Profile from "./pages/Profile";
 import FriendProfile from "./pages/FriendProfile";
+import Routes from './pages/Routes';
 import NotFound from "./pages/NotFound";
 
 // Auth-protected route

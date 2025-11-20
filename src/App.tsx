9e9f9340d00ef6c31f0ec01703084095323026dc
raydalessandro <alessio.marrone1993@gmail.com>
2025-11-20 22:32:08 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import Routes from './pages/Routes'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
